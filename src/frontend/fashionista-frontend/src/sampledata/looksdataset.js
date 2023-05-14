590364ec9dd6ffb1cd2355dea028323700778a56
Jruz9 <josec300@live.com>
2023-05-14 17:15:55 -0400

    Added changes to looksdatasets

--- src/frontend/fashionista-frontend/src/sampledata/looksdataset.js
+++ src/frontend/fashionista-frontend/src/sampledata/looksdataset.js
@@ -1,1 +1,8 @@
 //this is made up of columns types: looks name
+// Looks can also be renamed as outfits
+
+export const looksDataset= [
+    {name:"black one"},
+    {name:"black 2"},
+    {name:"summer casual blue"}
+]
