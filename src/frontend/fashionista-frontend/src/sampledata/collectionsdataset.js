b46350d0c6fa6d80fd19396936e0ee30bba109f4
Jruz9 <josec300@live.com>
2023-05-15 17:34:48 -0400

    updated sample datasets for looks and collections

--- src/frontend/fashionista-frontend/src/sampledata/collectionsdataset.js
+++ src/frontend/fashionista-frontend/src/sampledata/collectionsdataset.js
@@ -1,1 +1,17 @@
 //  collection dataset is made up of columns:collection name
+
+
+export const collectionDataset = [
+    {
+        name: "summer collection", looksList: [
+            { id: 1, name: "beach clothes 1" },
+            { id: 2, name: "beach clothes 2" },
+            { id: 3, name: "beach clothes 3" }]
+    },
+    {
+        name: "winter collection", looksList:
+            [{ id: 4, name: "carmelTrenchcoat" },
+            { id: 5, name: "puffer white style" },
+            { id: 6, name: "netflix chill 1" }]
+    }
+]
--- src/frontend/fashionista-frontend/src/sampledata/looksdataset.js
+++ src/frontend/fashionista-frontend/src/sampledata/looksdataset.js
@@ -1,8 +1,10 @@
-//this is made up of columns types: looks name
+//this is made up of columns types: looks name and clothing list
+//clothing list is dynamic so people can choose what parts they want 
+//from 1 to 10 of whatever withd defaults of 3 minimium and additional clothing from the front end. 
 // Looks can also be renamed as outfits
 
-export const looksDataset= [
-    {name:"black one"},
-    {name:"black 2"},
-    {name:"summer casual blue"}
+export const looksDataset = [
+    { name: "black one", clothingList: { pant: "501 jeans", shirt: "black hm shirt", shoes: "black combs" } },
+    { name: "black 2", clothingList: { pant: "north face black sweatpant", shirt: "black nasa shirt", shoes: "black s1 nikes" } },
+    { name: "summer casual blue", clothingList: { pant: "lulu white shorts", shirt: "button down blue plaid shirt", shirt: "white undershirt", shoes: "brown crocs" } }
 ]
